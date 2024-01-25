3e6404c46110cc7542de815e281ce72601477983
LingnaC <che23173@uvg.edu.gt>
2024-01-24 19:10:13 -0600

    Quick sort

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,35 @@
+/*Universidad del Valle de Guatemala
+Algoritmos y estruccturas de datos
+Linda Chen 23173
+Mia Fuentes 23775*/
+
 public class QuickSort {
-    
+    public static void sort(Comparable[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(Comparable[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(Comparable[] arr, int low, int high) {
+        Comparable pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j].compareTo(pivot) <= 0) {
+                i++;
+                Comparable temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        Comparable temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
 }
